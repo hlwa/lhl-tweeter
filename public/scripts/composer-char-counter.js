65dd20ef6a11fc537791e2b981aca77e5f893ba5
Hailan Wang <hailan6257@email.com>
2021-12-14 19:03:14 -0500

    Modified the event

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,19 @@
 // ensure the DOM has loaded.
 $(document).ready(function() {
   // register an event to the input area;
-  $("#tweet-text").on('keypress', function() {
+  $("#tweet-text").on('keypress', function() {//better add another event just in case user paste some content in
     const inputLengh = $(this).val().length + 1;
     const counter = $(this).parent().get(0).counter;
-    counter.innerText = inputLengh;
+
+    counter.innerText = inputLengh > 140 ? 140 - inputLengh : inputLengh;
   });
+
+  // $("#tweet-text").on('focusout', function() {
+  //   const inputLengh = $(this).val().length + 1;
+  //   const counter = $(this).parent().get(0).counter;
+  //   if (inputLengh > 140) {
+  //     counter.innerText = 140 - inputLengh;
+  //   }
+  //   counter.innerText = inputLengh;
+  // });
 });
