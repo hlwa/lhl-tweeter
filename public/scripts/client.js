c7887e1b4d97a6a2a8360c7500c383414d96d8c8
Hailan Wang <hailan6257@email.com>
2021-12-15 21:34:51 -0500

    Add AJAX get and some conditions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,31 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
 
 const createTweetElement = tweetData => {
   let tweetElement = `
@@ -56,9 +31,6 @@ const createTweetElement = tweetData => {
   return tweetElement;
 };
 
-// Test / driver code (temporary). Eventually will get this from the server.
-
-
 
 const renderTweets = function(tweets) {
   // loops through tweets
@@ -71,41 +43,49 @@ const renderTweets = function(tweets) {
   }
 };
 
-$(document).ready(function() {//#tweet-text
-  
+//call to post tweets
+const sentTweets = () => {
   $("#form").submit(function(event) {
     event.preventDefault();
-    //const textI = $(this).children('#tweet-text').val();
     const $tweet = $(this).serialize();
+    const tweetContent = $tweet.split('=')[1];
+    if (tweetContent === '' || tweetContent === null) {
+      alert('Content is empty!');
+      return;
+    }
+    if (tweetContent.length > 140) {
+      alert(`Content is too long`);
+      return;
+    }
+    //post data to sever
     $.post($tweet);
-    console.log($tweet);
   });
+};
 
-  const loadTweets = () => {
-  // sending the requet to the TV Maze API
-    const url = 'http://localhost:8080/tweets';
-    $.ajax({
-      url: url,
-      method: 'GET',
+//call to get and show tweets
+const loadTweets = () => {
+  // sending the requet to /tweets
+  const url = 'http://localhost:8080/tweets';
+  $.ajax({
+    url: url,
+    method: 'GET',
+  })
+    .done((results) => {
+      console.log(results);
+      renderTweets(results);
     })
-      .done((results) => {
-        console.log(results); // array of objects
-        renderTweets(results);
+    .fail((err) => {
+      console.log(`Error: ${err.message}`);
+    })
+    .always(() => {
+      console.log('request to http://localhost:8080/tweets done');
+    });
+};
 
-      // with the results => create the HTML element => attach to the DOM
-      })
-      .fail((err) => {
-        console.log(`Error: ${err.message}`);
-      })
-      .always(() => {
-        console.log('request to http://localhost:8080/tweets done');
-      });
-    loadTweets();//call to load
-  };
-  
-
-  
-
-  //renderTweets(data);
+$(document).ready(function() {
+  //call to post tweets
+  sentTweets();
+  //call to get and show tweets
+  loadTweets();
 });
 
