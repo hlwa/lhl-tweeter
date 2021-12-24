874440b20c88da7eef72886d4fe0c7002f3d0e0d
Hailan Wang <hailan6257@email.com>
2021-12-24 14:01:46 -0500

    improved the counter and the warning message

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,14 @@
 // ensure the DOM has loaded.
 $(document).ready(function() {
   // register an event to the input area;
-  $("#tweet-text").on('keypress', function() {//better add another event just in case user paste some content in
+  $("#tweet-text").on('keyup', function() {//better add another event just in case user paste some content in
     const inputLengh = $(this).val().length + 1;
     const counter = $(this).parent().get(0).counter;
+    $(counter).css('color', 'slategrey');
     //set the value of counter
     counter.innerText = 140 - inputLengh;
     if (counter.innerText < 0) {
       $(counter).css('color', 'red');
     }
-    
- 
   });
 });
