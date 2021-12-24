61b2fff9e35904a1b124b9bed627a5bb2974589d
Hailan Wang <hailan6257@email.com>
2021-12-24 14:34:43 -0500

    register action to the keyup, paste, cut event to the input area;

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,21 @@
 // ensure the DOM has loaded.
 $(document).ready(function() {
-  // register an event to the input area;
-  $("#tweet-text").on('keyup', function() {//better add another event just in case user paste some content in
-    const inputLengh = $(this).val().length + 1;
-    const counter = $(this).parent().get(0).counter;
+  
+  // define an action for the event;
+const textArea = $("#tweet-text");
+  const textAction = () =>{//better add another event just in case user paste some content in
+    const inputLengh = $("#tweet-text").val().length;
+    const counter = $("#tweet-text").parent().get(0).counter;
     $(counter).css('color', 'slategrey');
     //set the value of counter
     counter.innerText = 140 - inputLengh;
     if (counter.innerText < 0) {
       $(counter).css('color', 'red');
     }
-  });
+  };
+  // register action to the keyup, paste, cut event to the input area;
+  $("#tweet-text").on('keyup', textAction);
+  $("#tweet-text").bind('paste', textAction);
+  $("#tweet-text").bind('cut', textAction);
+
 });
